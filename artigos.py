d38df695a3276cb7eb65c7d44eab6e78ac114749
Rodrigo Schio <rodrigoschio3@gmail.com>
2019-04-25 18:35:40 -0400

    inicio

---
+++ artigos.py
@@ -0,0 +1,68 @@
+import numpy as np
+import random
+class Scheduler:
+    def __init__(self, crossoverrate, mutationrate, inputpath, maxgen=100):
+        self.crossoverrate = crossoverrate
+        self.mutationrate = mutationrate
+        self.maxgen = maxgen
+        self.matrixPath = inputpath 
+        self.loadDataFromFile()
+
+        self.lenReviewers = len(self.dataset)
+        self.lenPapers = len(self.dataset[0]) - 1 
+
+    def loadDataFromFile(self):
+        self.dataset = np.loadtxt(self.matrixPath, delimiter = ",")
+
+# distribute creates a random individual.
+def distribute(s):
+    # create a list of possible reviewers
+    # with his position at s.dataset and the
+    # number of papers can review.
+    possibleReviewers = list()
+    for i in range(0,len(s.dataset)):
+        r = [i, s.dataset[i][-1]] 
+        possibleReviewers.append(r)
+
+    l = list()
+    for i in range(s.lenPapers):
+        num = random.randint(0, len(possibleReviewers) - 1)
+        # get the position of reviwer at s.dataset.
+        l.append(possibleReviewers[num][0])
+        possibleReviewers[num][1] -= 1
+
+        if possibleReviewers[num][1] <= 0:
+            possibleReviewers.remove(possibleReviewers[num])
+    return l 
+
+def fitness(s, distribution):
+    val = 0
+    for i in range(0, len(distribution)):
+        r = distribution[i]
+        val += s.dataset[r][i]
+    return val
+
+def crossover(s, d1, d2):
+    cutPoint = int(s.crossoverrate * len(d1))
+    d3 = list()
+    d4 = list()
+    for i in range(0, cutPoint):
+        d3.append(d1[i])
+        d4.append(d2[i])
+    for i in range(cutPoint, len(d1)): 
+        d3.append(d2[i])
+        d4.append(d1[i])
+    return [d3, d4]
+
+
+if __name__ == "__main__":
+    s = Scheduler(0.7, 1, "matrix.txt")
+    l1 = distribute(s)
+    l2 = distribute(s)
+    f = fitness(s, l1)
+    print(l1)
+    print(l2)
+    print(f)
+    ls = crossover(s, l1, l2)
+    print(ls[0])
+    print(ls[1])
---
+++ genMatrix.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"math/rand"
+)
+
+func main() {
+	p := flag.Int("p", 5, "number of papers")
+	r := flag.Int("r", 5, "number of reviewers")
+	f := flag.String("f", "matrix.txt", "output file name")
+	flag.Parse()
+
+	if *p < 1 || *r < 1 {
+		return
+	}
+	matrix := genMatrix(*r, *p)
+
+	file, err := os.Create(*f)
+	if err != nil {
+		log.Fatalf("failed to create file: %s", *f)
+		return
+	}
+	defer file.Close()
+	file.Write(intMatrix(matrix).ByteSlice())
+}
+
+type intMatrix [][]int
+
+func (m intMatrix) String() string {
+	return string(m.ByteSlice())
+}
+
+func (m intMatrix) ByteSlice() []byte {
+	// Create a sufficient big slice to avoid
+	// syscalls the great enemy of speed.
+	bs := make([]byte, 0, len(m)*len(m[0])*3)
+	for i := 0; i < len(m); i++ {
+		for _, n := range m[i][:len(m[i])-1] {
+			// Use the fast way:
+			// We know that this number is in [0, 5]
+			// so just add the number to byte '0'.
+			bs = append(bs, byte('0'+n))
+			bs = append(bs, ',')
+		}
+		// The last number can be bigger than 9 so we need
+		// to user Itoa to convert it.
+		bs = append(bs, strconv.Itoa(m[i][len(m[i])-1])...)
+		bs = append(bs, '\n')
+	}
+	return bs
+}
+
+func doWork(matrix [][]int, done chan<- int, sReviewers, eReviewers, nPapers int) {
+	totalOfReviews := 0
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := sReviewers; i < eReviewers; i++ {
+		matrix[i] = make([]int, nPapers+1)
+		for j := 0; j < nPapers; j++ {
+			matrix[i][j] = random.Intn(6)
+		}
+		matrix[i][nPapers] = random.Intn(nPapers) + 1
+		totalOfReviews += matrix[i][nPapers]
+	}
+	done <- totalOfReviews
+}
+
+func genMatrix(nReviewers, nPapers int) [][]int {
+	matrix := make([][]int, nReviewers)
+	totalOfReviews := 0
+	div := 8
+	done := make(chan int)
+	sReviewers := 0
+	eReviewers := nReviewers / div
+
+	for i := 0; i < div-1; i++ {
+		go doWork(matrix, done, sReviewers, eReviewers, nPapers)
+		sReviewers = eReviewers
+		eReviewers += nReviewers / div
+	}
+	go doWork(matrix, done, sReviewers, nReviewers, nPapers)
+
+	for i := 0; i < div; i++ {
+		totalOfReviews += <-done
+	}
+
+	if totalOfReviews < nPapers {
+		matrix[nReviewers-1][nPapers] += nPapers - totalOfReviews
+	}
+	return matrix
+}
