e3107ed7b888aea4b863d8fd9b80028024109267
Claudio-Padilha <padilha86@gmail.com>
2019-04-27 02:28:00 -0400

    changed quite a bit, have backup

--- artigos.py
+++ artigos.py
@@ -15,8 +15,13 @@ class Scheduler:
     def loadDataFromFile(self):
         self.dataset = np.loadtxt(self.matrixPath, delimiter = ",")
 
-# distribute creates a random individual.
-def distribute(s):
+class Individual:
+    def __init__(self, distribution, fit):
+        self.distribution = distribution
+        self.fit = fit
+
+# creates a random state.
+def distribution(s):
     # create a list of possible reviewers
     # with his position at s.dataset and the
     # number of papers can review.
@@ -25,23 +30,33 @@ def distribute(s):
         r = [i, s.dataset[i][-1]] 
         possibleReviewers.append(r)
 
-    l = list()
+    ret = list()
     for i in range(s.lenPapers):
         num = random.randint(0, len(possibleReviewers) - 1)
         # get the position of reviwer at s.dataset.
-        l.append(possibleReviewers[num][0])
+        ret.append(possibleReviewers[num][0])
         possibleReviewers[num][1] -= 1
 
         if possibleReviewers[num][1] <= 0:
             possibleReviewers.remove(possibleReviewers[num])
-    return l 
+    return ret 
 
-def fitness(s, distribution):
+# fitness determines fitness function value
+def fitness(s, distribution):                                   
     val = 0
     for i in range(0, len(distribution)):
         r = distribution[i]
         val += s.dataset[r][i]
     return val
+
+# create our inicial random set
+def createFirstPopulation (s):
+    population = list()
+    for i in range(s.lenPapers): 
+        dist = distribution(s)                                                  # Creates a population of the size of lenPapers   ##### TRY TO CHANGE THIS VALUE LATER  ########
+        element = Individual(dist, fitness(s, dist))
+        population.append(element)                                              # Adds the new individual to the population
+    return population.sort(key=lambda individual: individual.fit, reverse=True) # This is sorted (greatest fitness to smallest)
 
 # checkReviewers check if distribution respects
 # the max number of papers that reviewers can take.
@@ -57,103 +72,51 @@ def checkReviewers(s, distribution):
             return False
     return True
     
-def crossover(s, d1, d2):
-    cutPoint = int(s.crossoverrate * len(d1))
-    d3 = list()
-    d4 = list()
-    for i in range(0, cutPoint):
-        d3.append(d1[i])
-        d4.append(d2[i])
-    for i in range(cutPoint, len(d1)): 
-        d3.append(d2[i])
-        d4.append(d1[i])
-    ds = [d3, d4]
-    return ds
-
-def mutate(s, d):
-    x = random.randint(0,100)/100.0
-    if x <= s.mutationrate:
+def crossover(s, parent1, parent2):  
+    descendents = [parent1, parent2]                                              # Assumes crossover won't happen
+    x = random.randint(0, 100) /100.0
+    if x <= s.crossoverrate:                                                      # Verifies if crossover will happen
         valid = False
-        while (! valid):
-            p = random.randint(0, len(d) - 1)
-            n = random.randint(0,5)
-            d[p] = n
-            valid = checkReviewers(s, d)
+        i = 0
+        while not valid and i in range(100):                                       # crossover parents until both children are valid or for 100 times 
+            cutPoint = random.randint(0,len(parent1)-1)                           # Determines cutting point
+            child1 = parent1[0 : cutPoint] + parent2[cutPoint : len(parent2)]      
+            valid = checkReviewers(s, child1)                                     # verifies if the first mutation was valid
+            child2 = parent2[0 : cutPoint] + parent1[cutPoint : len(parent1)]     
+            valid = checkReviewers(s, child2)                                     # verifies if the second mutation was valid
+            i+=1
+        
+        if valid:
+            descendents = [child1, child2]                                        # updates the descendents if a valid state was generated
+    return descendents
 
+def mutate(s, d): 
+    x = random.randint(0,100)/100.0
+    if x <= s.mutationrate:                                     # Determines if the mutation will happen
+        valid = False
+        while not valid:                                        # generate new states until one of them is valid
+            aux = d
+            p = random.randint(0, len(d) - 1)                   # Choses a position on the state   
+            n = random.randint(0,5)                             # choses a random reviewer
+            aux[p] = n                                          # Creates new possible mutated state
+            valid = checkReviewers(s, aux)                      # verifies if the mutation was valid
+        d = aux                                                 # mutates the state
     return d
 
-def crossAndMutate(s, d1, d2):
-    ds = crossover(s, d1, d2)
-    ds[0] = mutate(s, ds[0])
-    ds[1] = mutate(s, ds[1])
-    ret = []
-    if checkReviewers(s, ds[0]):
-        ret.append(ds[0])
-    if checkReviewers(s, ds[1]):
-        ret.append(ds[1])
-    return ret
-
-    
-class HeapElem:
-    def __init__(self, pos, fit):
-       self.pos = pos 
-       self.fit = fit
-    def __cmp__(self, other):
-        return cmp(self.fit, other.fit)
-
-# TODO: verificar o que eh selecao por
-# roleta e implementar.
-def select(s, distributions):
-    h = list()
-    heapq.heapify(h)
-    for i in range(0, len(distributions)):
-        f = fitness(s, distributions[i])
-        e = HepElem(i, f)
-        heapq.heappush(h, e)
-        if len(h) > 200:
-            heapq.heappop(h)
-    selection = list()
-    for i in range(0, len(h)):
-        selection.append(distributions[h[i].pos])
-    return selection
-
-def genParents(s, number):
-    parents = list()
-    for i in range(0, number): 
-        parents.append(distribute(s))
-    return parents
-    
-def genChildren(s, parents):
-    children = list()
-    for i in range(0, len(parents)):
-        for j in range(0, len(parents)):
-            if i == j:
-                continue
-            cs = crossAndMutate(s, parents[i], parents[j])
-            for k in range(0, len(cs)):
-                children.append(cs[k])
+def crossAndMutate(s, parent1, parent2):
+    children = crossover(s, parent1, parent2)
+    children[0] = mutate(s, children[0])
+    children[1] = mutate(s, children[1])
     return children
 
-def generationFit(s, distributions):
-    maxFit = 0
-    for i in range(0, len(distributions)):
-        f = fitness(s, distributions[i])
-        if f > maxFit:
-            maxFit = f
-    return maxFit
+def select(s, population):
+    #TODO: function to select the pairs to crossover, returns  a list of individuals
+
+def reproduce(s, population):
+    newPopu = list()
+    newPopu.append(population[0])                   # sends the individual with the highest fit to the next generation
+    #TODO: function to create the next generetion
 
 if __name__ == "__main__":
-    s = Scheduler(0.7, 0.3, "matrix.txt")
+    s = Scheduler(0.7, 0.1, "matrix.txt")
     
-    parents = genParents(s, 200)
-    gensFit = list() 
-    for i in range(0, 20):
-        children = genChildren(s, parents)
-        print("children:")
-        print(len(children))
-        parents = select(s, children)
-        gensFit.append(generationFit(s, parents))
-        #s.crossoverrate = random.uniform(0.6, 1)
-
-    for i in range(0, len(gensFit)):
-        print(gensFit[i])
